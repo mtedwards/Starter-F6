b77be72b3d41b8480d5a29a0d0ce126243050186
Matt Edwards <matt@emptyhead.com.au>
2015-11-14 12:46:04 +1100

    Fix up the loop

--- index.php
+++ index.php
@@ -15,24 +15,24 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-		<?php if ( have_posts() ) : ?>
+		<?php if ( have_posts() ) { ?>
       
 			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php while ( have_posts() ) { the_post(); ?>
 
 				<?php
 					the_content();
 				?>
 
-			<?php endwhile; ?>
+			<?php } // endwhile; ?>
 
 			<?php the_posts_navigation(); ?>
 
-		<?php else : ?>
+		<?php } else { ?>
 
-			<?php get_template_part( 'content', 'none' ); ?>
-
-		<?php endif; ?>
+      			<p>Oops. There is nothing here.</p>
+      
+		<?php } //endif; ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
